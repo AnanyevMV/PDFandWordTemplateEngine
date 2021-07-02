276e8c0b0b76cab1aa55bc3f2bd1a8d390cde1c0
Mihail Ananyev <mihail.ananev123@gmail.com>
2021-07-02 19:00:39 +0300

    Added PDF and Word file downloading

--- src/main/java/com/ananyevmv/controller/RestTemplateEngineController.java
+++ src/main/java/com/ananyevmv/controller/RestTemplateEngineController.java
@@ -5,12 +5,19 @@ import com.ananyevmv.service.TemplateEngineService;
 import com.ananyevmv.service.TextToPDFService;
 import com.ananyevmv.service.TextToWordService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @RestController
 @RequestMapping("/api")
@@ -29,20 +36,42 @@ public class RestTemplateEngineController {
     }
 
     @PostMapping("/saveToPDF")
-    public ResponseEntity<?> saveToPDF(@RequestBody TemplateInput templateInput) {
-        String templateResult = templateEngineService.processTemplate(templateInput);
-        
-        String fileName = textToPDFService.convertStringToPDF(templateResult);
+    public ResponseEntity<Resource> saveToPDF(@RequestBody TemplateInput templateInput) {
+        try {
+            String templateResult = templateEngineService.processTemplate(templateInput);
 
-        return new ResponseEntity<>(templateResult, HttpStatus.OK);
+            String fileName = textToPDFService.convertStringToPDF(templateResult);
+
+            ByteArrayResource byteArrayResource = new ByteArrayResource(Files.readAllBytes(Path.of(fileName)));
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=output.pdf");
+
+            return ResponseEntity.ok().headers(headers).contentLength(byteArrayResource.contentLength()).
+                    contentType(MediaType.APPLICATION_OCTET_STREAM).body(byteArrayResource);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @PostMapping("/saveToWord")
     public ResponseEntity<?> saveToWord(@RequestBody TemplateInput templateInput) {
-        String templateResult = templateEngineService.processTemplate(templateInput);
+        try {
+            String templateResult = templateEngineService.processTemplate(templateInput);
 
-        String fileName = textToWordService.convertStringToWord(templateResult);
+            String fileName = textToWordService.convertStringToWord(templateResult);
 
-        return new ResponseEntity<>(templateResult, HttpStatus.OK);
+            ByteArrayResource byteArrayResource = new ByteArrayResource(Files.readAllBytes(Path.of(fileName)));
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=output.docx");
+
+            return ResponseEntity.ok().headers(headers).contentLength(byteArrayResource.contentLength()).
+                    contentType(MediaType.APPLICATION_OCTET_STREAM).body(byteArrayResource);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
