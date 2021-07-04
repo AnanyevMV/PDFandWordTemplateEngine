9a073b6035b3ffbbd308018acc70c99e25d3be35
Mihail Ananyev <mihail.ananev123@gmail.com>
2021-07-04 21:32:21 +0300

    Small unit testing of TemplateEngineService

--- src/test/java/com/ananyevmv/service/TemplateEngineServiceImplTest.java
+++ src/test/java/com/ananyevmv/service/TemplateEngineServiceImplTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class TemplateEngineServiceImplTest {
@@ -15,13 +16,32 @@ class TemplateEngineServiceImplTest {
     @Autowired
     private TemplateEngineService templateEngineService;
 
-    @Test()
-    void testIfExceptionIsThrownWhenSubstitutionVariableIsMissing() {
+    @Test
+    public void testIfExceptionIsThrownWhenSubstitutionVariableIsMissing() {
         assertThrows(RuntimeException.class, () -> {
             TemplateInput templateInput = new TemplateInput();
             templateInput.setTemplate("${name} ${lastName}");
             templateInput.setSubstitutions(Map.of("name", "John", "LASTNAME", "Doe"));
-            String result = templateEngineService.processTemplate(templateInput);;
+            String result = templateEngineService.processTemplate(templateInput);
         });
+    }
+
+    @Test
+    public void testIfExceptionIsThrownWhenSubstitutionValueContainsIllegalSymbols() {
+        assertThrows(RuntimeException.class, () -> {
+            TemplateInput templateInput = new TemplateInput();
+            templateInput.setTemplate("${name} ${lastName}");
+            templateInput.setSubstitutions(Map.of("name", "John", "lastName", "abcdefgh${lastName}"));
+            String result = templateEngineService.processTemplate(templateInput);
+        });
+    }
+
+    @Test
+    public void testIfTemplateResultEqualToCorrectValue() {
+        TemplateInput templateInput = new TemplateInput();
+        templateInput.setTemplate("${name} ${lastName}");
+        templateInput.setSubstitutions(Map.of("name", "John", "lastName", "Doe"));
+        String result = templateEngineService.processTemplate(templateInput);
+        assertEquals(result, "John Doe");
     }
 }
